2e5394c0d79630661146ba9a0f32c7c27b466491
janath-kumar <110004562+janath-kumar@users.noreply.github.com>
2022-09-28 12:27:17 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "AWSEC2Instance"{
      count   = 1
      ami     =  "ami-05c8ca4485f8b138a"
      instance_type = "t2.micro"
-     security_groups = ["launch-wizard-2"]
+     security_groups = ["launch-wizard-17"]
      key_name        = "janathkumar"
      tags = {
         Name = "tomcatservers"
